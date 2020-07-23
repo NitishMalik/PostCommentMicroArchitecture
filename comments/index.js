72d933f19678f7d4e4cef69c0d623bc74e1a94d8
malik.nitish@gmail.com <malik.nitish@gmail.com>
2020-07-23 09:42:38 +0530

    Adding cors functionality in post and comment service

--- comments/index.js
+++ comments/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 
 const commentsByPost = {};
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPost[req.params.id] || []);
